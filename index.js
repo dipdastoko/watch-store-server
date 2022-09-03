53b9da02cb2a85a9151bab52b69689958eed04e6
dipdastoko <dipdastoko@gmail.com>
2022-09-03 06:04:56 +0600

    apis created

--- index.js
+++ index.js
@@ -5,6 +5,61 @@ require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zqquk.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('watch-store');
+        const usersCollection = database.collection('users');
+
+        app.get('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email };
+            const user = await usersCollection.findOne(query);
+            let admin = false;
+            if (user?.role === 'admin') {
+                admin = true;
+            }
+            console.log(query);
+            res.json(admin);
+        })
+
+        app.post('/user', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.json(result);
+        });
+
+        app.put('/user', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        });
+
+        app.put('/user/makeadmin', async (req, res) => {
+            const email = req.body.email;
+            const filter = { email };
+            const updateDoc = { $set: { role: 'admin' } };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            console.log(email);
+            res.json(result);
+        });
+
+    }
+    finally {
+        // await client.close();
+    }
+
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('watch store server');
